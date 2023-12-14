f87969477caceb1e7ab31c852a2cddffa9b6b630
siiimba31 <elena.chelle31@gmail.com>
2023-12-14 21:18:30 +0100

    2eme graphes ok

--- index.js
+++ index.js
@@ -103,6 +103,8 @@ function getWorkExp(jsonData) {
     return workExps;
 }
 
+
+
 //récupère tous les niveau d'études présent dans le json data
 function getEdLevel(jsonData) {
     let edLevel = [];
@@ -132,6 +134,24 @@ function getPlatformWork(jsonData) {
     return platformeWork;
 }
 
+//récupère les différentes frameworks
+function getFramework(jsonData) {
+    let frameworks = [];
+    for (let i = 0; i < jsonData.length; i++) {
+        //si des frameworks sont renseignées
+        if (jsonData[i]["WebframeHaveWorkedWith"] != 'NA') {
+            let framework = (jsonData[i]["WebframeHaveWorkedWith"]).split(";");
+            //pour chaque frameworks renseigné
+            for (let j = 0; j < framework.length; j++) {
+                if (!frameworks.includes(framework[j])) {
+                    frameworks.push(framework[j]);
+                }
+            }
+        }
+    }
+    return frameworks;
+}
+
 //Récupère les données correspondant au pays "country" du json data
 function getDataCountry(country, jsonData) {
     let data = [];
@@ -147,8 +167,8 @@ function getDataCountry(country, jsonData) {
 function getDataCountryWorkExp(country, workExp, jsonData) {
     let datas = getDataCountry(country,jsonData);
     let data = [];
-    for (let i = 0; i < jsonData.length; i++) {
-        if (datas[i]["WorkExp"] === workExp) {
+    for (let i = 0; i < datas.length; i++) {
+        if (Number(datas[i]["WorkExp"]) === Number(workExp)) {
             data.push(datas[i]);
         }
     } 
@@ -341,7 +361,7 @@ function loadChartRMNE(years, values, country) {
 }
 
 //FONCTION POUR LE TROISIEME GRAPHE
-//renvoie le salaire moyen par année d'expérience 
+//renvoie le salaire moyen par platformes et âge 
 function calculMoyenneSalaireParPlatforme(jsonData, platformeWork) {
     let dict = {};
     let compteur = {};
@@ -371,8 +391,12 @@ function calculMoyenneSalaireParPlatforme(jsonData, platformeWork) {
 }
 
 //mise ne forme des données pour le chart
-function revenueMoyenParCloud(country, jsonData) {
-    datas = getDataCountry(country, jsonData);
+function revenueMoyenParCloud(country, workExp, jsonData) {
+    if (workExp=="ALL"){
+        datas=getDataCountry(country, jsonData);
+    }else{
+        datas=getDataCountryWorkExp(country, workExp, jsonData); getDataCountry(country, jsonData);
+    };
     platformeWork = getPlatformWork(jsonData);
     moyennesSalaires = calculMoyenneSalaireParPlatforme(datas, platformeWork);
     values = [];
@@ -383,61 +407,200 @@ function revenueMoyenParCloud(country, jsonData) {
 }
 
 //mise a jour du chart en fonction de la country
-function updateCountryRMPC(chart, jsonData, country) {
+function updateCountryRMPC(chart, jsonData, country, workExp) {
 
     chart.data.datasets = [{
         label: 'Moyenne des salaires par niveau d\'étude pour :' + country,
-        data: revenueMoyenParCloud(country, jsonData)["tableauSalaires"]
+        data: revenueMoyenParCloud(country, workExp, jsonData)["tableauSalaires"]
     }]
     chart.update();
 }
 
 //créer le selecteur de country
-function createCountriesDropDownRMPC(divSelector, countries, myChart, jsonData) {
-    var existingDropDown = divSelector.querySelector("select");
-    if (existingDropDown) {
-        divSelector.removeChild(existingDropDown);
+function createCountriesDropDownRMPC(divSelector1, divSelector2, countries, workExp, myChart, jsonData) {
+    var existingDropDown1 = divSelector1.querySelector("select");
+    if (existingDropDown1) {
+        divSelector1.removeChild(existingDropDown1);
     }
 
-    let dropDown = document.createElement("select")
+    let dropDown1 = document.createElement("select");
     for (let country of countries) {
         let option = document.createElement('option');
         option.value = country;
         option.text = country;
         // On ajoute l'option au dropDown
-        dropDown.appendChild(option);
+        dropDown1.appendChild(option);
     }
-    divSelector.appendChild(dropDown);
-    dropDown.addEventListener("change", (event) => {
-        country = dropDown.options[dropDown.selectedIndex].value
-        updateCountryRMPC(myChart, jsonData, country);
-    });
-}
+    divSelector1.appendChild(dropDown1);
 
-//créer le selecteur de country
-function createCountriesDropDownWorkExpRMPC(divSelector, workExps, myChart, jsonData) {
-    var existingDropDown = divSelector.querySelector("select");
-    if (existingDropDown) {
-        divSelector.removeChild(existingDropDown);
+    var existingDropDown2 = divSelector2.querySelector("select");
+    if (existingDropDown2) {
+        divSelector2.removeChild(existingDropDown2);
     }
 
-    let dropDown = document.createElement("select")
-    for (let workExp of workExps) {
+    let dropDown2 = document.createElement("select");
+
+    let option = document.createElement('option');
+    option.value = "ALL";
+    option.text = "ALL";
+    // On ajoute l'option au dropDown
+    dropDown2.appendChild(option);
+
+    for (let exp of workExp.sort((function (a, b) { return a - b;}))) {
         let option = document.createElement('option');
-        option.value = workExp;
-        option.text = workExp;
+        option.value = exp;
+        option.text = exp;
         // On ajoute l'option au dropDown
-        dropDown.appendChild(option);
+        dropDown2.appendChild(option);
     }
-    divSelector.appendChild(dropDown);
-    dropDown.addEventListener("change", (event) => {
-        country = dropDown.options[dropDown.selectedIndex].value
-        updateCountryRMNE(myChart, jsonData, country);
+    divSelector2.appendChild(dropDown2);
+
+    dropDown1.addEventListener("change", (event) => {
+        country = dropDown1.options[dropDown1.selectedIndex].value
+        workExp = dropDown2.options[dropDown2.selectedIndex].value
+        updateCountryRMPC(myChart, jsonData, country, workExp);
+    });
+
+    dropDown2.addEventListener("change", (event) => {
+        country = dropDown1.options[dropDown1.selectedIndex].value
+        workExp = dropDown2.options[dropDown2.selectedIndex].value
+        updateCountryRMPC(myChart, jsonData, country, workExp);
     });
 }
 
 //rencoie la configuration du chart pour le créer 
 function loadChartRMPC(years, values, country) {
+    const data = {
+        labels: years,
+        datasets: [{
+            label: 'Revenue moyen en fonction des plateformes de cloud pour : ' + country,
+            data: values
+        }]
+    }
+    const config = {
+        type: 'bar',
+        data: data,
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    }
+    return config;
+}
+
+//FONCTION POUR LE QUATRIEME GRAPHE
+//renvoie le salaire moyen par framework
+function calculMoyenneSalaireParFramwork(jsonData, frameworks) {
+    let dict = {};
+    let compteur = {};
+    for (let i = 0; i < frameworks.length; i++) {
+        dict[frameworks[i]] = 0;
+        compteur[frameworks[i]] = 0;
+    }
+    //somme des salaire
+    for (let j = 0; j < jsonData.length; j++) {
+        if (jsonData[j]["WebframeHaveWorkedWith"] != 'NA') {
+            let framework = (jsonData[j]["WebframeHaveWorkedWith"]).split(";");
+            //pour chaque framework renseigné
+            for (let l = 0; l < framework.length; l++) {
+                if (frameworks.includes(framework[l]) && jsonData[j]["CompTotal"] != 'NA') {
+                    valeurConverti = convertEnEuro(jsonData[j]["Currency"], jsonData[j]["CompTotal"]);
+                    dict[framework[l]] += valeurConverti;
+                    compteur[framework[l]] += 1;
+                }
+            }
+        }
+    }
+    //diviser pour optenir la moyenne
+    for (let k = 0; k < Object.keys(dict).length; k++) {
+        dict[frameworks[k]] = dict[frameworks[k]] / compteur[frameworks[k]];
+    }
+    return dict;
+}
+
+//mise ne forme des données pour le chart
+function revenueMoyenParFramework(country, workExp, jsonData) {
+    if (workExp=="ALL"){
+        datas=getDataCountry(country, jsonData);
+    }else{
+        datas=getDataCountryWorkExp(country, workExp, jsonData); getDataCountry(country, jsonData);
+    };
+    frameworks = getFramework(jsonData);
+    moyennesSalaires = calculMoyenneSalaireParFramwork(datas, frameworks);
+    values = [];
+    for (let i = 0; i < frameworks.length; i++) {
+        values.push(moyennesSalaires[frameworks[i]]);
+    }
+    return { "tableauNiveau": frameworks, "tableauSalaires": values };
+}
+
+//mise a jour du chart en fonction de la country
+function updateCountryRMFD(chart, jsonData, country, workExp) {
+
+    chart.data.datasets = [{
+        label: 'Moyenne des salaires par niveau d\'étude pour :' + country,
+        data: revenueMoyenParFramework(country, workExp, jsonData)["tableauSalaires"]
+    }]
+    chart.update();
+}
+
+//créer le selecteur de country
+function createCountriesDropDownRMFD(divSelector1, divSelector2, countries, workExp, myChart, jsonData) {
+    var existingDropDown1 = divSelector1.querySelector("select");
+    if (existingDropDown1) {
+        divSelector1.removeChild(existingDropDown1);
+    }
+
+    let dropDown1 = document.createElement("select");
+    for (let country of countries) {
+        let option = document.createElement('option');
+        option.value = country;
+        option.text = country;
+        // On ajoute l'option au dropDown
+        dropDown1.appendChild(option);
+    }
+    divSelector1.appendChild(dropDown1);
+
+    var existingDropDown2 = divSelector2.querySelector("select");
+    if (existingDropDown2) {
+        divSelector2.removeChild(existingDropDown2);
+    }
+
+    let dropDown2 = document.createElement("select");
+
+    let option = document.createElement('option');
+    option.value = "ALL";
+    option.text = "ALL";
+    // On ajoute l'option au dropDown
+    dropDown2.appendChild(option);
+
+    for (let exp of workExp.sort((function (a, b) { return a - b;}))) {
+        let option = document.createElement('option');
+        option.value = exp;
+        option.text = exp;
+        // On ajoute l'option au dropDown
+        dropDown2.appendChild(option);
+    }
+    divSelector2.appendChild(dropDown2);
+
+    dropDown1.addEventListener("change", (event) => {
+        country = dropDown1.options[dropDown1.selectedIndex].value
+        workExp = dropDown2.options[dropDown2.selectedIndex].value
+        updateCountryRMFD(myChart, jsonData, country, workExp);
+    });
+
+    dropDown2.addEventListener("change", (event) => {
+        country = dropDown1.options[dropDown1.selectedIndex].value
+        workExp = dropDown2.options[dropDown2.selectedIndex].value
+        updateCountryRMFD(myChart, jsonData, country, workExp);
+    });
+}
+
+//rencoie la configuration du chart pour le créer 
+function loadChartRMFD(years, values, country) {
     const data = {
         labels: years,
         datasets: [{
@@ -506,8 +669,10 @@ function execussionPage(request) {
         createCountriesDropDownRMNE(divSelectorRMNE, countries, ChartRMNE, jsonData);
 
         //TROISIEME CHART
-        cloud = revenueMoyenParCloud(country, jsonData)["tableauNiveau"];
-        valuesRMPC = revenueMoyenParCloud(country, jsonData)["tableauSalaires"];
+        let workExp = getWorkExp(jsonData);
+        let exp="ALL"
+        cloud = revenueMoyenParCloud(country, exp, jsonData)["tableauNiveau"];
+        valuesRMPC = revenueMoyenParCloud(country, exp, jsonData)["tableauSalaires"];
         configRMPC = loadChartRMPC(cloud, valuesRMPC, country);
 
         var canvasRMPC = document.getElementById("ChartRMPC");
@@ -521,7 +686,27 @@ function execussionPage(request) {
             ChartRMPC = new Chart(ctx3, configRMPC);
         }
         const divSelectorCountryRMPC = document.getElementById("selectorCountryRMPC");
-        createCountriesDropDownRMPC(divSelectorCountryRMPC, countries, ChartRMPC, jsonData);
+        const divSelectorAnneExpRMPC = document.getElementById("selectorAnneeExpRMPC");
+        createCountriesDropDownRMPC(divSelectorCountryRMPC, divSelectorAnneExpRMPC, countries, workExp, ChartRMPC, jsonData);
+
+        //QUATRIEME CHART
+        framework = revenueMoyenParFramework(country, exp, jsonData)["tableauNiveau"];
+        valuesRMFD = revenueMoyenParFramework(country, exp, jsonData)["tableauSalaires"];
+        configRMFD = loadChartRMFD(framework, valuesRMFD, country);
+
+        var canvasRMFD = document.getElementById("ChartRMFD");
+        if (canvasRMFD) {
+            var ctx4 = canvasRMFD.getContext('2d');
+            var existingChart4 = Chart.getChart(ctx4);
+
+            if (existingChart4) {
+                existingChart4.destroy();
+            }
+            ChartRMFD = new Chart(ctx4, configRMFD);
+        }
+        const divSelectorCountryRMFD = document.getElementById("selectorCountryRMFD");
+        const divSelectorAnneExpRMFD = document.getElementById("selectorAnneeExpRMFD");
+        createCountriesDropDownRMFD(divSelectorCountryRMFD, divSelectorAnneExpRMFD, countries, workExp, ChartRMFD, jsonData);
     })
 }
 
